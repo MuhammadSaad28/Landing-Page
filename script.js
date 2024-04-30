1d079478453960d95a2a3020760bb6183f3f6a64
MuhammadSaad28 <muhammadsaad28102003@gmail.com>
2024-04-30 18:36:10 +0500

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+// Typing effect
+var typed = new Typed('#typed', {
+    strings: ['educating voters','conducting surveys','making polls', 'running contests','selecting leaders ', 'making decisions', 'team collaboration'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
